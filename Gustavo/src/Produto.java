d930fee9b5fd1f51ab4018b741ad7fca85e28597
Lucas <lucas.espiritosanto@ufrpe.br>
2025-06-19 04:27:29 -0300

    .

--- Gustavo/src/Produto.java
+++ Gustavo/src/Produto.java
@@ -1,11 +1,54 @@
 public class Produto {
     private String nome;
     private String descricao;
-    private double precoMedio;
     private String categoria;
     private Produtor produtor;
 
-    public Produtor produtorAssociado(){
+    public Produto(String nome, String descricao, String categoria, Produtor produtor)
+    {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.categoria = categoria;
+        this.produtor = produtor;
+    }
+
+    public String getNome()
+    {
+        return nome;
+    }
+    public void setNome(String nome)
+    {
+        this.nome = nome;
+    }
+    public String getDescricao()
+    {
+        return descricao;
+    }
+    public void setDescricao(String descricao)
+    {
+        this.descricao = descricao;
+    }
+    public String getCategoria()
+    {
+        return categoria;
+    }
+    public void setCategoria(String categoria)
+    {
+        this.categoria = categoria;
+    }
+    public Produtor getProdutor()
+    {
         return produtor;
+    }
+    public void setProdutor(Produtor produtor)
+    {
+        this.produtor = produtor;
+    }
+
+
+
+    public Produtor produtorAssociado(Produtor produtor)
+    {
+
     }
 }
---
+++ Gustavo/src/ProdutorRepositorio.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class ProdutorRepositorio
+{
+    private ArrayList<Produtor> produtores;
+    public ProdutorRepositorio (int capacidadeInicial)
+    {
+        this.produtores = new ArrayList<>(capacidadeInicial);
+    }
+
+    public boolean AddProdutor(Produtor p)
+    {
+        boolean resultado = false;
+        if (p != null)
+        {
+            String cpfCnpj = p.getCpfCnpj();
+            boolean existeprodutor = false;
+
+            for(Produtor i : produtores)
+            {
+                if (i.getCpfCnpj().equals(cpfCnpj))
+                {
+                    existeprodutor = true;
+                    break;
+                }
+            }
+
+            if (!existeprodutor)
+            {
+                this.produtores.add(p);
+                resultado = true;
+            }
+        }
+        return resultado;
+    }
+}
